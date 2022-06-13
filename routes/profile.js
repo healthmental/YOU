7c11dccbfe22907d343c0fed581664a636d4c539
4mohamedsalem <4mohamedsalem@gmail.com>
2022-06-13 21:30:40 +0200

    profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,36 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Doctor = require("../models/doctor");
+const profile = require("../controllers/profile");
 
-router.get("/user/:userId/profile", async (req, res, next) => {
-  try {
-    const userId = req.params.userId;
-    const user = await User.findById(userId, { password: 0 });
-    console.log(user);
-    if (!user) {
-      const error = new Error("A user with this id could not be found.");
-      error.statusCode = 401;
-      throw error;
-    }
-    return res.status(200).json({ user });
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/user/:userId/profile", profile.getProfileUser);
 
-router.get("/doctor/:doctorId/profile", async (req, res, next) => {
-  try {
-    const doctorId = req.params.doctorId;
-    const doctor = await Doctor.findById(doctorId, { password: 0 });
-    if (!doctor) {
-      const error = new Error("A user with this id could not be found.");
-      error.statusCode = 401;
-      throw error;
-    }
-    return res.status(200).json({ doctor });
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/doctor/:doctorId/profile", profile.getProfileDoctor);
 
 module.exports = router;
